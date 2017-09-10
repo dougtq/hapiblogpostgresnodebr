284979c89793e57cf11755a011c16c96a090d528
Douglas Alves <douglas.expalves@gmail.com>
2017-09-10 12:25:29 -0300

    uuid, bcrypt and password visbility

--- libs/db.js
+++ libs/db.js
@@ -1,4 +1,13 @@
 'use strict'
-const knexfile = require('../knexfile')
-const knex = require('knex')(knexfile[process.env.NODE_ENV || 'development' ])
-let book = require('bookshelf')(knex)
+let knexfile = require('../knexfile')
+let knex = require('knex')(knexfile[process.env.NODE_ENV || 'development'])
+let bookshelf = require('bookshelf')(knex)
+let uuid = require('bookshelf-uuid')
+let bcrypt = require('bookshelf-bcrypt')
+
+bookshelf.plugin(uuid)
+bookshelf.plugin(bcrypt)
+bookshelf.plugin('visibility')
+
+module.exports = bookshelf
+
